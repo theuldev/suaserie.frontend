e2142b27874fdc7ed90f4e0e30c4c72dae097a7b
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-06-01 17:32:27 -0300

    Inputs animados feitos.

---
+++ src/components/InputFloat/InputFloat.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import './InputFloat.css'
+
+
+
+
+
+
+
+
+const InputFloat = (props) => {
+
+  // Input animation
+  const [isActive, setIsActive] = useState(false);
+  const [value, setValue] = useState('');
+
+  function handleTextChange(text) {
+    setValue(text);
+
+    if (text !== '') {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }
+
+  return (
+    <div className="input-form inputBox">
+    <input className="inputFloat"  type={props.inputType} value={value}
+      onChange={(e) => handleTextChange(e.target.value)} required />
+    <label className={isActive ? "active" : ""} htmlFor="inputFloat"  >{props.inputName}</label>
+  </div>
+  )
+
+};
+
+
+
+export default InputFloat;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import './Login.css'
 import logos from '../../constants/logos';
@@ -6,68 +6,75 @@ import logos from '../../constants/logos';
 // ícones:
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { Link } from "react-router-dom";
+import InputFloat from "../../components/InputFloat/InputFloat";
 
 
-const Login = () => (
+
+
+const Login = () => {
+
   
-  <div className="body-login">
-    <Helmet>
-      <title>Só Séries - Login</title>
+  return (
+    <div className="body-login">
+      <Helmet>
+        <title>Só Séries - Login</title>
 
-    </Helmet>
+      </Helmet>
 
-    <div className="container-login">
-      <div className="row">
+      <div className="container-login">
+        <div className="row">
 
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <div className="login_box">
-            <div className="login_header">
-              <div className="header-logo-login">
-                <img className="img-logo-login" src={logos.logoLight}
-                  alt="Projeto SóSeries" />
-
-              </div>
-              <h1>Login</h1>
-            </div>
-            <form className="form_login">
-              <div className="email-input-form">
-                <label className="email_label" htmlFor="email_input">E-mail</label>
-                <input className="email_input" type="email" placeholder="exemplo@gmail.com" required />
-              </div>
-
-              <div className="password-input-form">
-                <label className="password_label" htmlFor="password_input">Senha</label>
-                <input className="password_input" type="password" placeholder=" * * * * * * * * *" required />
-              </div>
-              <div className="password_recovery">
-                <a href="#">Esqueceu a senha?</a>
-              </div>
-              <div className="form-btn-login">
-                <button className="btn-login"><span className="material-symbols-outlined">
-                  <BsFillArrowRightCircleFill />
-                </span></button>
-              </div>
-              <div className="sign-up">
-                <span>Ainda não possui nenhuma conta? <Link className="sign-up-link" to={'/cadastro'}>Crie uma nova
-                  conta</Link></span>
-              </div>
-
-
-
-            </form>
-
-          </div>
         </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="login_box">
+              <div className="login_header">
+                <div className="header-logo-login">
+                  <img className="img-logo-login" src={logos.logoDark}
+                    alt="Projeto SóSeries" />
 
+                </div>
+                <h1>Login</h1>
+              </div>
+              <form className="form_login">
+
+           <InputFloat inputType="email" inputName="E-mail"  />
+           <InputFloat inputType="password" inputName="Senha"  />
+
+
+            
+
+
+
+                <div className="password_recovery">
+                  <a href="#">Esqueceu a senha?</a>
+                </div>
+                <div className="form-btn-login">
+                  <button className="btn-login"><span className="material-symbols-outlined">
+                    <BsFillArrowRightCircleFill />
+                  </span></button>
+                </div>
+                <div className="sign-up">
+                  <span>Ainda não possui nenhuma conta? <Link className="sign-up-link" to={'/cadastro'}>Crie uma nova
+                    conta</Link></span>
+                </div>
+
+
+
+              </form>
+
+            </div>
+          </div>
+
+        </div>
       </div>
+
+
+
     </div>
+  )
 
-
-
-  </div>
-);
+};
 
 
 
