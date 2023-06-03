0042aa4a74636a0681b5518e68719e0552d43fb3
lstheus <matheus18sdl@outlook.com>
2023-06-02 23:24:16 -0300

    Alterando o design dos Inputs

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,81 +1,64 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import './Login.css'
-import logos from '../../constants/logos';
+import "./Login.css";
+import logos from "../../constants/logos";
 
 // ícones:
-import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import InputFloat from "../../components/InputFloat/InputFloat";
 
-
-
-
 const Login = () => {
-
-  
   return (
     <div className="body-login">
       <Helmet>
         <title>Só Séries - Login</title>
-
       </Helmet>
 
       <div className="container-login">
-        <div className="row">
-
-        </div>
+        <div className="row"></div>
         <div className="row">
           <div className="col-12">
             <div className="login_box">
               <div className="login_header">
                 <div className="header-logo-login">
-                  <img className="img-logo-login" src={logos.logoDark}
-                    alt="Projeto SóSeries" />
-
+                  <img
+                    className="img-logo-login"
+                    src={logos.logoLight}
+                    alt="Projeto SóSeries"
+                  />
                 </div>
                 <h1>Login</h1>
               </div>
               <form className="form_login">
-
-           <InputFloat inputType="email" inputName="E-mail"  />
-           <InputFloat inputType="password" inputName="Senha"  />
-
-
-            
-
-
+                <InputFloat inputType="email" inputName="E-mail" />
+                <InputFloat inputType="password" inputName="Senha" />
 
                 <div className="password_recovery">
                   <a href="#">Esqueceu a senha?</a>
                 </div>
                 <div className="form-btn-login">
-                  <button className="btn-login"><span className="material-symbols-outlined">
-                    <BsFillArrowRightCircleFill />
-                  </span></button>
+                  <button className="btn-login">
+                    <span className="material-symbols-outlined">
+                      <BsFillArrowRightCircleFill />
+                    </span>
+                  </button>
                 </div>
                 <div className="sign-up">
-                  <span>Ainda não possui nenhuma conta? <Link className="sign-up-link" to={'/cadastro'}>Crie uma nova
-                    conta</Link></span>
+                  <span>
+                    Ainda não possui nenhuma conta?{" "}
+                    <Link className="sign-up-link" to={"/cadastro"}>
+                      Crie uma nova conta
+                    </Link>
+                  </span>
                 </div>
-
-
-
               </form>
-
             </div>
           </div>
-
         </div>
       </div>
-
-
-
     </div>
-  )
-
+  );
 };
-
-
 
 export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,115 +1,72 @@
-import React from "react";
+import {React} from "react";
 import { Helmet } from "react-helmet";
-import './Register.css'
-import logos from '../../constants/logos';
-import images from '../../constants/images';
-
+import "./Register.css";
+import logos from "../../constants/logos";
+import images from "../../constants/images";
 
 // ícones:
-import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import InputFloat from "../../components/InputFloat/InputFloat";
+
 
 const Register = () => (
-
   <div className="body-register">
     <Helmet>
       <title>Só Séries - Cadastro</title>
-
     </Helmet>
-
 
     <div className="container">
       <div className="form-logo">
-        <img
-          src={logos.logoDark}
-          alt="Projeto SóSeries"
-        />
+        <img src={logos.logoDark} alt="Projeto SóSeries" />
       </div>
       <div className="form">
         <form action="#">
           <div className="form-header">
-            <h1>Cadastre-se <br /><span>É rápido e fácil.</span></h1>
+            <h1>
+              Cadastre-se <br />
+              <span>É rápido e fácil.</span>
+            </h1>
             <div className="title"></div>
           </div>
           <div className="profile-pic">
             <div className="profile-input-form">
               <div className="wrapper-profile">
-                <img
-                  src={images.user}
-                  alt="Foto do usuário"
-                  id="photo"
-                />
+                <img src={images.user} alt="Foto do usuário" id="photo" />
                 <input type="file" id="file" required />
-                <label htmlFor="file" id="uploadPicBtn">Escolha foto</label>
+                <label htmlFor="file" id="uploadPicBtn">
+                  Escolha foto
+                </label>
               </div>
             </div>
           </div>
 
           <div className="input-group">
             <div className="input-box">
-              <label htmlFor="firstname">Primeiro Nome</label>
-              <input
-                id="firstname"
-                type="text"
-                name="firstname"
-                placeholder="Primeiro nome"
-                required
-              />
+              <InputFloat inputType="Text" inputName="Primeiro Nome" />
             </div>
 
             <div className="input-box">
-              <label htmlFor="lastname" >Ultimo Nome</label>
-              <input
-                id="lastname"
-                type="text"
-                name="lastname"
-                placeholder="Ultimo nome"
-                required
-              />
+              <InputFloat inputType="Text" inputName="Ultimo Nome" />
             </div>
 
             <div className="input-box">
-              <label htmlFor="email">E-mail</label>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="exemple@soseries.com"
-                required
+              <InputFloat
+                inputType="Email"
+                inputName="E-mail"
               />
+            </div>
+            <div className="input-box">
+                 <InputFloat inputType="Text" inputName="Apelido" />
             </div>
 
             <div className="input-box">
-              <label htmlFor="apelido">Apelido</label>
-              <input
-                id="apelido"
-                type="text"
-                name="apelido"
-                placeholder="Apelido"
-                required
-              />
+                 <InputFloat inputType="Password" id="input-password" inputName="Senha" />
             </div>
 
             <div className="input-box">
-              <label htmlFor="password">Senha</label>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                placeholder="* * * * * * * * * * * * *"
-                required
-              />
-            </div>
-
-            <div className="input-box">
-              <label htmlFor="ConfirmPassword">Confirme sua Senha</label>
-              <input
-                id="ConfirmPassword"
-                type="password"
-                name="ConfirmPassword"
-                placeholder="Confirme sua senha"
-                required
-              />
+    
+                 <InputFloat inputType="Password" inputName="Confirme sua Senha" />
             </div>
           </div>
 
@@ -118,16 +75,14 @@ const Register = () => (
           </div>
           <div className="conexao">
             Já possui uma conta?
-          
-            <Link to={'/login'}   className="conexao-link"  >Conecte-se.</Link>
+            <Link to={"/login"} className="conexao-link">
+              Conecte-se.
+            </Link>
           </div>
         </form>
       </div>
     </div>
-
   </div>
 );
-
-
 
 export default Register;
