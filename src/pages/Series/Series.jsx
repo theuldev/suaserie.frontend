ff09941d892f28ab0bdd0fa859c665a6e264bff2
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-06-04 17:22:07 -0300

    Consertos leves e base da página Séries

---
+++ src/pages/Series/Series.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
+import "./Series.css";
+import logos from "../../constants/logos";
+
+// ícones:
+
+// Componentes:
+
+
+const Series = () => {
+  return (
+    <div className="body-Series">
+      <Helmet>
+        <title>Só Séries - Series</title>
+      </Helmet>
+
+    
+    </div>
+  );
+};
+
+export default Series;
