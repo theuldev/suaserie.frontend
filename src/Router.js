9bca510089d1fb39fd1faf6c3393ed6eb8e44c2f
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-06-11 16:03:16 -0300

    modificação do modelo de tela das telas gerais de séries.

--- src/Router.js
+++ src/Router.js
@@ -7,10 +7,10 @@ import InitialPage from './pages/InitialPage/InitialPage.jsx';
 import Login from './pages/Login/Login.jsx';
 import Register from './pages/Register/Register.jsx';
 import Profile from "./pages/Profile/Profile";
-import Series from "./pages/Series/Series";
 import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
 import SerieView from './pages/SerieView/SerieView.jsx';
 import SerieSelected from './pages/SerieSelected/SerieSelected.jsx';
+import ScreenModel from './components/General/ScreenModel/ScreenModel.jsx';
 
 // Rotas:
 export const router = createBrowserRouter([
@@ -41,7 +41,7 @@ export const router = createBrowserRouter([
   },
   {
     path: '/series',
-    element: <Series/>,
+    element: <ScreenModel/>,
     // children: [
 
     // ]   
